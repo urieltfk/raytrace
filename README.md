aa63f715f9aef9847a67036e64649519e18f5b95
uriel <urieltfk@gmail.com>
2024-03-19 00:02:46 +0200

    improved hit_sphere fucntion

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ https://raytracing.github.io/books/RayTracingInOneWeekend.html
 Other sources used: 
 https://www.scratchapixel.com/lessons/3d-basic-rendering/introduction-to-ray-tracing/implementing-the-raytracing-algorithm.html
 
+https://pragprog.com/titles/jbtracer/the-ray-tracer-challenge/
+
 # compiling:
 To compile type make and press enter.
 
--- main.cpp
+++ main.cpp
@@ -4,7 +4,28 @@
 #include "vec3.h"
 #include "ray.h"
 
+double hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = half_b*half_b - a*c;
+
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-half_b - sqrt(discriminant) ) / a;
+    }
+}
+
 color ray_color(const ray& r) {
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0)
+    {
+        vec3 n = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(n.x() + 1, n.y() + 1, n.z() + 1);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
 

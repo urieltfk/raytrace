8398d9d477b696108ef02f3e816088280aa36c0c
uriel <urieltfk@gmail.com>
2024-03-23 19:45:37 +0200

    added interval.h and interval class

--- sphere.h
+++ sphere.h
@@ -3,18 +3,19 @@
 
 #include "vec3.h"
 #include "hittable.h"
+#include "interval.h"
 
 class sphere: public hittable {
 public:
     sphere(const point3& center, double radius): center(center), radius(radius) {}
 
-    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override;
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const override;
 private:
     point3 center;
     double radius;
 };
 
-bool sphere::hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const {
+bool sphere::hit(const ray& r, interval ray_t, hit_record& rec) const {
     vec3 oc = r.origin() - center;
     auto a = r.direction().length_squared();
     auto half_b = dot(oc, r.direction());
@@ -24,13 +25,13 @@ bool sphere::hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec
     if (discriminant < 0) {
         return false;
     }
-
     auto sqrtd = sqrt(discriminant);
+
     // Find the nearest root that lies in the acceptable range.
     auto root = (-half_b - sqrtd) / a;
-    if (root <= ray_tmin || ray_tmax <= root) {
+    if (!ray_t.surrounds(root)) {
         root = (-half_b + sqrtd) / a;
-        if (root <= ray_tmin || ray_tmax <= root)
+        if (!ray_t.surrounds(root))
             return false;
     }
 

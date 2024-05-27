9cdc712a8fbb24f83cb575d05b0bdd26fb8398b2
uriel <urieltfk@gmail.com>
2024-05-28 01:14:01 +0300

    added antialiasing

--- color.h
+++ color.h
@@ -8,9 +8,16 @@
 using color = vec3;
 
 void write_color(std::ostream &out, color pixel_color) {
-    out << static_cast<int>(255.999 * pixel_color.x()) << ' '
-        << static_cast<int>(255.999 * pixel_color.y()) << ' '
-        << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+    
+    static const interval intensity(0.000, 0.999);
+    int rbyte = int(256 * intensity.clamp(r));
+    int gbyte = int(256 * intensity.clamp(g));
+    int bbyte = int(256 * intensity.clamp(b));
+
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
 
 #endif // COLOR_H

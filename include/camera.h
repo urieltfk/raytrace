c0c49a064f215267d08c09a5a2c87135af8d652f
uriel <urieltfk@gmail.com>
2024-05-30 00:42:38 +0300

    added max depth

--- include/camera.h
+++ include/camera.h
@@ -10,6 +10,7 @@ public:
     double aspect_ratio = 1.0; // Ratio of image width over height
     int    image_width = 100;  // Rendered image width in pixel count
     int samples_per_pixel = 10; // Count of random samples for each pixel;
+    int max_depth = 10; // Maximum number of rays bounce into scene
 
 void render(const hittable& world) {
     initialize();
@@ -22,7 +23,7 @@ void render(const hittable& world) {
             color pixel_color(0, 0, 0);
             for (int sample = 0; sample < samples_per_pixel; sample++) {
                 ray r = get_ray(i, j);
-                pixel_color += ray_color(r, world); // Interesting, see additon of colors.
+                pixel_color += ray_color(r, max_depth, world); // Interesting, see additon of colors.
             }
             write_color(std::cout, pixel_color * pixel_samples_scale);
         }
@@ -85,12 +86,15 @@ vec3 sample_square() const {
     return vec3(random_double() - 0.5, random_double() - 0.5, 0);
 }
 
-color ray_color(const ray& r, const hittable& world) const {
+color ray_color(const ray& r, int depth, const hittable& world) const {
+    if (depth <= 0) // If we've exceeded the ray bounce limit, no more light is gathered (prevent very long recursion)
+        return color(0, 0, 0);
+    
     hit_record rec;
 
     if (world.hit(r, interval(0, infinity), rec)) {
         vec3 direction = random_on_hemisphere(rec.normal);
-        return 0.5 * ray_color(ray(rec.p, direction), world);
+        return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
